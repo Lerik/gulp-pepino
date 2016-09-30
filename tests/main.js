d2121f8bd5ee99892039998300896ee25f9a17dd
William Avila <william.zx1@gmail.com>
2016-09-30 17:01:25 -0600

    working on the runchimp spec

--- tests/main.js
+++ tests/main.js
@@ -4,6 +4,7 @@ var assert = require('chai').assert;
 var expect = require('chai').expect;
 var fs = require('fs');
 var pepino = require('pepino-lib');
+var proc = require('child_process');
 
 describe('Gulp Pepino', function() {
     
@@ -17,16 +18,20 @@ describe('Gulp Pepino', function() {
     describe('When translating step file', function(){
         var contentRead = 'ContentToBeTranslated';
         var contentTranslated = 'ContentToBeWritten';
-        var stub_fs_readFileSync, stub_pepino_convert;
+        var stdOut = {on:function(){}};
+        var stub_fs_readFileSync, stub_pepino_convert, stub_proc_exec;
 
         beforeEach(function() {
             stub_fs_readFileSync = sinon.stub(fs, 'readFileSync');
             stub_pepino_convert = sinon.stub(pepino, 'convert');
+            stub_proc_exec = sinon.stub(proc, 'exec');
+
         });
 
         afterEach(function() {
             stub_fs_readFileSync.restore();
             stub_pepino_convert.restore();
+            stub_proc_exec.restore();
         });
 
         it('should translate the step file', function() {
@@ -40,6 +45,11 @@ describe('Gulp Pepino', function() {
             expect(contentWritten).to.be.equal(contentTranslated);
         });
 
+        it('should run chimp', function(){
+            
+            gpepino.runChimp(path.substring(0, path.lastIndexOf('/')), {});
+            sinon.assert.called(proc.exec);
+        });
 
 
     });

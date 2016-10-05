c1a5b9f707c9b439800012cb251e9ed8dcb00521
Lerik <jj.cob@unitec.edu>
2016-10-05 12:21:59 -0600

    Removing chimp functions

--- index.js
+++ index.js
@@ -9,40 +9,6 @@ var through = require('through2');
 
 const PLUGIN_NAME = 'gulp-pepino';
 
-var browsers = {
-    CH: 'chrome',
-    IE: 'ie',
-    FX: 'firefox',
-    SF: 'safari',
-    PJ: 'phantomjs'
-};
-
-var chimpBrowserOptions = {
-  string: 'browser',
-  default: { browser: browsers.CH }
-};
-
-var runChimp = function(directory, browser) {
-    var browserSelected;
-    if (browser) {
-        browserSelected = browser;
-    } else {
-        browserSelected = chimpBrowserOptions.default.browser;
-    }
-
-    var child = exec(`chimp --path="` + directory + `" --browser=${browserSelected}`);
-
-    child.stdout.on('data', function(data) {
-        if (data && data !== '\n') {
-            console.log(data);
-        }
-    });
-
-    child.stderr.on('data', function(data) {
-        console.log(data);
-    });
-}
-
 var translateStepFile = function(stepFile) {
     console.log('Translating: ' + path.basename(stepFile.path));
     var content = stepFile.contents.toString('utf8');
@@ -57,36 +23,14 @@ var translateStepFile = function(stepFile) {
     console.log(path.basename(stepFile.path) + ' has been translated successfully.');
 }
 
-var pepino = function(browser) {
+var pepino = function() {
     return through({ objectMode: true }, function(stepFile, enc, callback) {
         translateStepFile(stepFile);
-        runChimp(path.dirname(stepFile.path), browser);
-        this.push(null);
-        callback();
-    });
-}
-
-var translateStepFileStream = function() {
-    return through({ objectMode: true }, function(stepFile, enc, callback) {
-        translateStepFile(stepFile);
-        this.push(null);
-        callback();
-    });
-}
-
-var runChimpStream = function(browser) {
-    return through({ objectMode: true }, function(stepFile, enc, callback) {
-        runChimp(stepFile.path, browser);
         this.push(null);
         callback();
     });
 }
 
 module.exports = {
-    translateStepFile: translateStepFile,
-    translateStepFileStream: translateStepFileStream,
-    runChimp: runChimp,
-    runChimpStream: runChimpStream,
-    pepino: pepino,
-    browsers: browsers
+    pepino: pepino
 };
